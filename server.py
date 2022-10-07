6d16d7b9e01a5312366a321f00eb6c6ddd739160
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-07 15:24:50 -0500

    encode message

--- server.py
+++ server.py
@@ -13,5 +13,5 @@ while True:
     client_socket, address = server_socket.accept()
     print(f'received connection from {address}')
     message = f'Hello! Thank you for connecting to the server.\r\n'
-    client_socket.send(message)
+    client_socket.send(message.encode('ascii'))
     client_socket.close()
