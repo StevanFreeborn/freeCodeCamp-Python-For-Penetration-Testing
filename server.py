fc46c7cdbf9c1dfba175ada00c7a0ca2adb548dc
Stevan Freeborn <sfreeborn7@gmail.com>
2022-10-07 15:22:06 -0500

    create server

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket
+
+server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 444
+
+server_socket.bind((host, port))
+
+server_socket.listen()
+
+while True:
+    client_socket, address = server_socket.accept()
+    print(f'received connection from {address}')
+    message = f'Hello! Thank you for connecting to the server.\r\n'
+    client_socket.close()
